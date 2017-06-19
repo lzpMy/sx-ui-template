dab815deb8853bb3572a2503f20727e18b8c109c
李兆鹏 <li_zp@suixingpay.com\lzp3309523>
2017-06-20 00:02:08 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var chalk = require('chalk');    //不同颜色的info
+var util = require('util');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');    //yeoman弹出框
+var path = require('path');
+var Reactpackage = yeoman.Base.extend({
+    info: function() {
+        this.log(chalk.green(
+            'I am going to build your app!'
+        ));
+    },
+    generateBasic: function() {  //按照自己的templates目录自定义
+        this.directory('src', 'src');    //拷贝目录
+        this.directory('builder', 'builder');
+        this.directory('local', 'local');
+        this.directory('public', 'public');
+        this.directory('static', 'static');
+        this.copy('package.json', 'package.json');   //拷贝文件
+        this.copy('index.html', 'index.html');
+        this.copy('README.md', 'README.md');
+        this.copy('postcss.config.js', 'postcss.config.js');
+        this.copy('yarn.lock', 'yarn.lock');
+        this.copy('.babelrc', '.babelrc');
+    },
+    generateClient: function() {
+        this.sourceRoot(path.join(__dirname, 'templates'));
+        this.destinationPath('./');
+    },
+    install: function() {      //安装依赖
+        this.installDependencies({
+            skipInstall: this.options['skip-install']
+        });
+    },
+    end: function() {
+        this.log(yosay(
+            'Your app has been created successfully!'
+        ));
+    }
+});
+module.exports = Reactpackage;
